f73ba232180ad4679df480aeff812d6602256d59
SAISURYA <saisurya015@gmail.com>
2025-05-15 16:03:34 +0530

    header section done

--- dashboard/src/components/header/index.js
+++ dashboard/src/components/header/index.js
@@ -1,9 +1,21 @@
 
 "use client";
 
+import { GlobalContext } from "@/context";
+import { useContext } from "react";
+
 export default function Header() {
 
-  return <div>
-    Header
-  </div>
+    const {sideBarOpen, setSideBarOpen} = useContext(GlobalContext);
+  
+
+  return <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-lg">
+    <div className="flex flex-grow items-center gap-2 justify-end py-4 px-4 shadow md:px-6 2xl:px-11">
+      <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
+        <button className="inline-flex items-center justify-center bg-black px-6 py-2 text-lg text-white font-medium tracking-wide uppercase" >{sideBarOpen ? "Hide Sidebar" : "Show Sidebar" }</button>
+      </div>
+        <button className="inline-flex items-center justify-center bg-black px-6 py-2 text-lg text-white font-medium tracking-wide uppercase">Login</button>
+      
+    </div>
+  </header>
 }
